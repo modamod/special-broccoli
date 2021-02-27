2da261d7b41d97ff24025d99dfaaf7312911bc3f
modamod <modhaffer.rahmani@gmail.com>
2021-02-27 10:58:31 +0100

    Just pushing stuff

--- sbroccoli/helper.py
+++ sbroccoli/helper.py
@@ -3,48 +3,52 @@
 
 import shelve
 import time
-from contextlib import ContextDecorator
+from contextlib import contextmanager
 from functools import partial, wraps
 from tempfile import gettempdir
+from typing import NoReturn, Sequence, Tuple, Generator
 
 import boto3
 import botocore
+import sys
+from invoke import context
 
 
-def aws_session(*args, **kwargs):
+def aws_session(*args: Sequence[str], **kwargs: Sequence[str]) -> boto3.session.Session:
+    ''' Helper function to get boto3 session object '''
     return boto3.session.Session(*args, **kwargs)
 
 
-class Timer(ContextDecorator):
-    ''' Context decorator to compute how much it took to execute a function '''
-    def __init__(self, func):
-        ''' Initializing Timer object '''
-        self.name = 'name'
-        self.start_time = 0
-        self.duration = 0
-        self.func = func
-    def __enter__(self):
-        ''' Overriding context manger enter method '''
-        print(f'Starting {self.name}')
-        self.start_time = time.time()
-        return self
-    def __exit__(self, *exp):
-        ''' Overriding context manager exit function '''
-        self.duration = time.time() - self.start_time
-        hours, minutes, seconds = self.duration_to_hms()
-        print(f'{self.name} took: {hours:.0f}h:{minutes:.0f}m:{seconds:.2f}s'.strip())
-        return False
+def duration_to_hms(duration: float) -> Tuple:
+    ''' Function that turns number of seconds into Number of hours, minutes and seconds'''
+    hours = duration // 3600
+    minutes = (duration - hours * 3600) // 60
+    seconds = duration - hours * 3600 - minutes * 60
+    return hours, minutes, seconds
 
-    def duration_to_hms(self):
-        ''' Function that turns number of seconds into Number of hours, minutes and seconds'''
-        hours = self.duration // 3600 if self.duration else 0
-        minutes = (self.duration - hours * 3600) // 60 if self.duration else 0
-        seconds = self.duration - hours * 3600 - minutes * 60 if self.duration else 0
-        return hours, minutes, seconds
+
+@contextmanager
+def timer(name: str = '') -> NoReturn:
+    ''' Timer contextx manager/decorator '''
+    sts = ets = diff = 0
+    try:
+        sts = time.time()
+        yield
+    finally:
+        ets = time.time()
+        diff = ets - sts
+        hours, minutes, seconds = duration_to_hms(diff)
+        print(f'{name} took: {hours:.0f}h:{minutes:.0f}m:{seconds:.2f}s')
 
 
 def get_value_from_key(items, key):
-    ''' Helper function to use with map '''
+    ''' Helper function to use with map
+        Args:
+            items: iteratable that we are going to get the value from
+            key: the key used to get its value from the iteratable.
+        Returns:
+            Object
+    '''
     for i in items:
         if i['Key'] == key:
             return i['Value']
@@ -56,6 +60,15 @@ def key_value_to_dict(items, key_name='Key', value_name='Value'):
 
 
 def cache(func):
+    ''' Function decorator to cache function result in disk
+
+        Args:
+            func: Callable that the decorator is going to cache its result in disk
+
+        Returns:
+            Callable
+    '''
+
     shelf = shelve.open(f'{gettempdir()}/{func.__name__}')
     @wraps(func)
     def inner(*args, **kwargs):
@@ -75,7 +88,8 @@ def cache(func):
 @cache
 def get_ec2_instances(*args, **kwargs):
     ''' Helper function to get'''
-    session = aws_session(profile_name=kwargs['profile_name'], region_name=kwargs['region_name'])
+    session = aws_session(
+        profile_name=kwargs['profile_name'], region_name=kwargs['region_name'])
     client = session.client('ec2')
     try:
         paginator = client.get_paginator("describe_instances")
@@ -86,11 +100,12 @@ def get_ec2_instances(*args, **kwargs):
                 # each reservation returns an array of instances
                 for instance in reservation["Instances"]:
                     instances.append(instance)
-    except botocore.exceptions.ClientError as e:
+    except botocore.exceptions.ClientError as exp:
         print('This is an exception')
-        print(e.operation_name)
-        exit(1)
+        print(exp.operation_name)
+        sys.exit(1)
     return instances
+
 
 def format_ec2_instances(instance):
     result = {
@@ -99,9 +114,48 @@ def format_ec2_instances(instance):
     }
     return result
 
-@Timer
+
+@timer(name='Main')
 def main():
     time.sleep(1)
+
+
+def accounts_regions_to_tuple(accounts: str, regions: str) -> Tuple:
+    ''' Function to map accounts to regions passed through command line
+
+        Args:
+            accounts: comma delimited lists of aws profile names
+                exmaple: --acounts stlb,stcs,sthybrid
+            regions: semicolon delimited list of comma delimited list to define region for each specified account
+                example: --regions "us-east-1;us-east-1,us-east-2-us-west-2;u-east-1,us-east-2,us-west-2;eu-central-1,ap-southeast-2"
+    '''
+    accounts_region_mapping = dict(zip(map(lambda acc: acc.strip(), accounts.split(
+        ',')), map(lambda reg: reg.split(','), regions.split(';'))))
+    for key, values in accounts_region_mapping.items():
+        for item in values:
+            yield (key, item)
+
+
+def prep_args(contxt: context.Context, accounts: str,
+              regions: str, filters: str, refresh: bool) -> Generator:
+    ''' Function to perare the arguments for list_all_instances task
+        Args:
+
+            contxt: Invoke task context object, needed to invoke the list_instances task
+            accounts: comma delimited lists of aws profile names
+                exmaple: --acounts stlb,stcs,sthybrid
+            regions: semicolon delimited list of comma delimited list to define region for each specified account
+                example: --regions "us-east-1;us-east-1,us-east-2-us-west-2;u-east-1,us-east-2,us-west-2;eu-central-1,ap-southeast-2"
+            filters: semicolon delimited list of Key=Values pair to define ec2 filters
+                example: --filters tag:CostCenter=CloudSuite Corporate Enterprise Edition,CloudSuite HealthCare,CloudSuite Corporate Base;tag:machineLabel=INFORBCMG01
+            refresh: Flag to refresh cached values
+
+        Returns:
+            Generator
+    '''
+
+    for account_region in accounts_regions_to_tuple(accounts, regions):
+        yield (contxt, account_region[0], account_region[1], filters, refresh)
 
 
 if __name__ == "__main__":
--- sbroccoli/tasks/aws.py
+++ sbroccoli/tasks/aws.py
@@ -2,22 +2,26 @@
 ''' AWS invoke tasks module '''
 
 from invoke import task, Collection
-from sbroccoli.helper import get_ec2_instances, format_ec2_instances, timer
+from sbroccoli.helper import get_ec2_instances, format_ec2_instances, timer, prep_args
 from tempfile import gettempdir
 import yaml
 from pprint import pprint
 from pathlib import Path
+from multiprocessing import Manager, Pool
+
+
 @task(help={
-        'aws_profile': 'AWS Profile name used in aws config file',
-        'region': 'AWS region name',
-        'filters': 'AWS compatible filter list of dicts'
-    }
+    'aws_profile': 'AWS Profile name used in aws config file',
+    'region': 'AWS region name',
+    'filters': 'AWS compatible filter list of dicts'
+}
 )
 def list_instances(c, aws_profile, region, filters=[], instance_ids=[], write_to_file=False, refresh=False):
     ''' Invoke task to list all instances in account and region with filters '''
     @timer(name='Getting EC2 instances')
     def inner():
-        result = get_ec2_instances(profile_name=aws_profile, region_name=region, Filters=filters, InstanceIds=instance_ids, refresh=refresh)
+        result = get_ec2_instances(profile_name=aws_profile, region_name=region,
+                                   Filters=filters, InstanceIds=instance_ids, refresh=refresh)
         if write_to_file:
             output_file_path = Path(f'{gettempdir()}/{aws_profile}/{region}/instances.yaml')
             output_file_path.parent.mkdir(parents=True, exist_ok=True)
@@ -28,3 +32,18 @@ def list_instances(c, aws_profile, region, filters=[], instance_ids=[], write_to
             pprint(list(map(format_ec2_instances, result)))
         return result
     inner()
+
+
+@task(help={
+    'accounts': 'comma delimited lists of aws profile names\
+                exmaple: --acounts stlb,stcs,sthybrid',
+    'regions': 'semicolon delimited list of comma delimited list to define region for each specified account\
+                example: --regions "us-east-1;us-east-1,us-east-2-us-west-2;u-east-1,us-east-2,us-west-2;eu-central-1,ap-southeast-2"',
+    'filters': 'semicolon delimited list of Key=Values pair to define ec2 filters\
+                example: --filters tag:CostCenter=CloudSuite Corporate Enterprise Edition,CloudSuite HealthCare,CloudSuite Corporate Base;tag:machineLabel=INFORBCMG01',
+    'refresh': 'Flag to refresh cached values'
+}
+)
+def list_all_instances(c, accounts, regions, filters=[], refresh=False):
+    ''' List all instances in specified account/regions mapping '''
+    print(tuple(prep_args(c, accounts, regions, filters, refresh)))
